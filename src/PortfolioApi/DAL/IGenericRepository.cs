4cc570b80bb3fc8ed1d694a437d1cd20735e0610
Tahjid <ta.mostafa@asthait.com>
2018-02-23 16:53:44 +0600

    s

---
+++ src/PortfolioApi/DAL/IGenericRepository.cs
@@ -0,0 +1,14 @@
+﻿using PortfolioApi.Models;
+using System.Linq;
+
+namespace PortfolioApi.DAL
+{
+    interface IGenericRepository
+    {
+        void Insert<TEntity>(TEntity entity) where TEntity : BaseClass;
+        void Delete<TEntity>(TEntity entity) where TEntity : BaseClass;
+        void Update<TEntity>(TEntity entity) where TEntity : BaseClass;
+        TEntity GetByID<TEntity>(object id) where TEntity : BaseClass;
+        IQueryable<TEntity> GetQuery<TEntity>() where TEntity : BaseClass;
+    }
+}
---
+++ src/PortfolioApi/DAL/PortfolioContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PortfolioApi.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PortfolioApi.DAL
+{
+    public class PortfolioContext: DbContext
+    {
+        public PortfolioContext() : base()
+        {
+
+        }
+
+        public DbSet<BaseClass> Classes { get; set; }
+    }
+}
