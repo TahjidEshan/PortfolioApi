4b07809ea896c7d6b571025accdd21528b2f7742
TahjidEshan <atahjid@gmail.com>
2018-02-28 00:41:01 +0600

    modelbuilders added

--- PortFolio2017/Controllers/PortfolioController.cs
+++ PortFolio2017/Controllers/PortfolioController.cs
@@ -1,10 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
-using PortFolio2017.DAL;
-using PortFolio2017.Models;
+﻿using Microsoft.AspNetCore.Mvc;
+using PortFolio2017.ViewModelBuilder;
 using PortFolio2017.Services;
 
 namespace PortFolio2017.Controllers
@@ -17,41 +12,11 @@ namespace PortFolio2017.Controllers
         {
             this.BaseService = BaseService;
         }
-        //[HttpGet]
-        //public IEnumerable<string> Get()
-        //{
-        //    return BaseService.GetAllEmails().Select(x=>x.EmailAddress);
-        //}
-
-        //// GET api/values/5
-        //[HttpGet("{id}")]
-        //public string Get(int id)
-        //{
-        //    return "value";
-        //}
-
-        //// POST api/values
-        //[HttpPost]
-        //public void Post([FromBody]string value)
-        //{
-        //}
-
-        //// PUT api/values/5
-        //[HttpPut("{id}")]
-        //public void Put(int id, [FromBody]string value)
-        //{
-        //}
-
-        //// DELETE api/values/5
-        //[HttpDelete("{id}")]
-        //public void Delete(int id)
-        //{
-        //}
         [Route("api/get-all-email")]
         [HttpGet]
-        public IEnumerable<string> GetAllEmail()
+        public JsonResult GetAllEmails()
         {
-            return BaseService.GetAllEmails().Select(x => x.EmailAddress);
+            return Json(BaseModelBuilder.GetEmails(BaseService));
         }
     }
 }
---
+++ PortFolio2017/ViewModels/BaseViewModel.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PortFolio2017.ViewModels
+{
+    public class BaseViewModel
+    {
+    }
+    public class AddressViewModel
+    {
+        public string HouseNumber { get; set; }
+        public string RoadNumber { get; set; }
+        public string City { get; set; }
+        public string Country { get; set; }
+    }
+    public class EmailViewModel
+    {
+        public IList<string> Emails { get; set; }
+    }
+    public class PhoneViewModel
+    {
+        public IList<string> Phones { get; set; }
+    }
+    public class AddressListViewModel
+    {
+        public IList<AddressViewModel> Addresses { get; set; }
+    }
+    public class SocialViewModel
+    {
+        public string Name { get; set; }
+        public string Url { get; set; }
+        public string Icon { get; set; }
+    }
+    public class HeadersViewModel
+    {
+        public IList<string> Headers { get; set; }
+    }
+    public class SocialIconsViewModel
+    {
+        public IList<SocialViewModel> Icons { get; set; }
+    }
+    public class MottosViewModel
+    {
+        public IList<string> Mottos { get; set; }
+    }
+    public class GeneralSkillViewModel
+    {
+        public string Name { get; set; }
+        public int Percentage { get; set; }
+    }
+    public class GeneralSkillsList
+    {
+        public IList<GeneralSkillViewModel> Skills { get; set; }
+    }
+    public class WorkViewModel
+    {
+        public string Title { get; set; }
+        public string Url { get; set; }
+        public string UrlText { get; set; }
+        public string Description { get; set; }
+        public DateTime FromDate { get; set; }
+        public DateTime ToDate { get; set; }
+        public bool CurrentlyEmployed { get; set; }
+    }
+    public class WorkHistoryViewModel
+    {
+        public IList<WorkViewModel> Works { get; set; }
+    }
+    public class EducationViewModel
+    {
+        public string Title { get; set; }
+        public string Url { get; set; }
+        public string UrlText { get; set; }
+        public string Description { get; set; }
+        public DateTime FromDate { get; set; }
+        public DateTime ToDate { get; set; }
+        public bool CurrentlyEnrolled { get; set; }
+    }
+    public class EducationHistoryViewModel
+    {
+        public IList<EducationViewModel> Educations { get; set; }
+    }
+    public class PublicationViewModel
+    {
+        public string Name { get; set; }
+        public string Url { get; set; }
+        public string UrlText { get; set; }
+        public string Description { get; set; }
+        public IList<string> Authors { get; set; }
+    }
+    public class PublicationListViewModel
+    {
+        public IList<PublicationViewModel> Publications { get; set; }
+    }
+    public class ExpertiseViewModel
+    {
+        public string Title { get; set; }
+        public string Details { get; set; }
+    }
+    public class ExpertiseListViewModel
+    {
+        public IList<ExpertiseViewModel> Expertise { get; set; }
+    }
+    public class SpecialSkillsViewModel
+    {
+        public string Title { get; set; }
+        public int Percentage { get; set; }
+    }
+    public class SpecialSkillsList
+    {
+        public IList<SpecialSkillsViewModel> Skills { get; set; }
+    }
+}
